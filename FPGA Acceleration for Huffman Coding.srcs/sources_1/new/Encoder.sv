e58563f7d8826e5f6b3509d121473eb75e9d34cf
Anirudhhan Raghuraman <85730720+Heatwave591@users.noreply.github.com>
2026-01-17 16:51:38 -0500

    Update encoder.sv
    Added config, in, out logic pins

--- FPGA Acceleration for Huffman Coding.srcs/sources_1/new/Encoder.sv
+++ FPGA Acceleration for Huffman Coding.srcs/sources_1/new/Encoder.sv
@@ -20,7 +20,31 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Encoder(
+module huffman_encoder (
+    input logic clk,
+    input logic rst,        // Active Low Reset (Reset when 0)
 
-    );
+    // Defining the lookup table
+    input logic        wr_en,           // These three lines will be used to fill up the dictionary
+    input logic [7:0]  addr_config,     // Huffman = Dynamic; addr_config holds address; data_config holds the corresponding symbol
+    input logic [19:0] data_config,     // My dumbass will forget why I defined this if I dont write this comment   
+                                        
+    // Input
+    input  logic [7:0]  symbol_in,
+    input  logic        symbol_valid,
+    output logic        symbol_ready,
+
+    // Output
+    output logic [31:0] stream_out,
+    output logic        stream_valid,
+    input  logic        stream_ready
+);
+
+logic [19:0] code_table [255:0];        // Defining LUT and writing into it
+
+    always_ff @(posedge clk) begin
+        if (wr_en) begin
+            code_table[addr_config] <= data_config;
+        end
+    end
 endmodule
